bae5b78b87b6eed97b5ba4189f3842a18da07a15
Kyle Westhaus <kwesthaus@users.noreply.github.com>
2019-04-08 23:27:12 -0400

    Updates GRC runfiles before removing from repo

--- grc/top_block.py
+++ grc/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Sat Jan  5 13:01:42 2019
+# Generated: Mon Apr  8 22:39:34 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -16,259 +16,104 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from PyQt4 import Qt
 from gnuradio import blocks
-from gnuradio import digital
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
-from gnuradio import qtgui
+from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
 from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import pmt
-import sip
-import sys
-from gnuradio import qtgui
+import wx
 
 
-class top_block(gr.top_block, Qt.QWidget):
+class top_block(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Top Block")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Top Block")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
-
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 100e5
-        self.xponder_freq = xponder_freq = 914889000
-        self.target_rate = target_rate = 5000000
-        self.reader_freq = reader_freq = 915750000
-        self.firdes_tap_low_xponder = firdes_tap_low_xponder = firdes.low_pass(1, samp_rate, 1000000, 100000, firdes.WIN_HAMMING, 6.76)
-        self.firdes_tap_low_reader = firdes_tap_low_reader = firdes.low_pass(1, samp_rate, 1000000, 100000, firdes.WIN_HAMMING, 6.76)
-        self.capture_freq = capture_freq = 9.130e8
+        self.target_rate = target_rate = 50000
+        self.target_freq = target_freq = 915750000
+        self.samp_rate = samp_rate = 1e7
+        self.capture_freq = capture_freq = 9.13e8
+        self.baud_rate = baud_rate = 500000
 
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-        	8192, #size
-        	target_rate, #samp_rate
-        	"", #name
-        	1 #number of inputs
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=capture_freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=True,
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1.2, 1.2)
-
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(-1, True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_NORM, qtgui.TRIG_SLOPE_POS, 0.30, 0.00005, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(True)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
-
-        if not True:
-          self.qtgui_time_sink_x_0.disable_legend()
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "blue"]
-        styles = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-                   -1, -1, -1, -1, -1]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-        	1024, #size
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	capture_freq, #fc
-        	target_rate, #bw
-        	"", #name
-        	1 #number of inputs
-        )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(True)
-
-        if not True:
-          self.qtgui_freq_sink_x_0.disable_legend()
-
-        if "complex" == "float" or "complex" == "msg_float":
-          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.freq_xlating_fir_filter_xxx_1_0 = filter.freq_xlating_fir_filter_ccc(int(samp_rate / target_rate), (firdes_tap_low_xponder), -(capture_freq - xponder_freq ), samp_rate)
-        self.digital_clock_recovery_mm_xx_0 = digital.clock_recovery_mm_ff(5.0*(1+0.0), 0.25*0.175*0.175, 0.5, 0.175, 0.1)
-        self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
-        self.blocks_throttle_1 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.Add(self.wxgui_fftsink2_0.win)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vff((1000, ))
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/orville/Documents/cybersecurity/kudu/easypass_captures/provided/3/before/rfcomms.cfile', False)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/orville/Documents/tech/projects/cybersecurity/rf/kudu/easypass_captures/provided/1/during/rfcomms.cfile', False)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/orville/Documents/cybersecurity/kudu/easypass_captures/provided/3/before/grc.bytes', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
-        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
-        self.blocks_add_const_vxx_0 = blocks.add_const_vff((-1.1, ))
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_add_const_vxx_0, 0), (self.digital_clock_recovery_mm_xx_0, 0))
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_1, 0))
-        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.blocks_add_const_vxx_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.freq_xlating_fir_filter_xxx_1_0, 0))
-        self.connect((self.blocks_throttle_1, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.digital_clock_recovery_mm_xx_0, 0), (self.digital_binary_slicer_fb_0, 0))
-        self.connect((self.digital_clock_recovery_mm_xx_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.freq_xlating_fir_filter_xxx_1_0, 0), (self.blocks_complex_to_mag_0, 0))
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "top_block")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.set_firdes_tap_low_xponder(firdes.low_pass(1, self.samp_rate, 1000000, 100000, firdes.WIN_HAMMING, 6.76))
-        self.set_firdes_tap_low_reader(firdes.low_pass(1, self.samp_rate, 1000000, 100000, firdes.WIN_HAMMING, 6.76))
-        self.blocks_throttle_1.set_sample_rate(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-
-    def get_xponder_freq(self):
-        return self.xponder_freq
-
-    def set_xponder_freq(self, xponder_freq):
-        self.xponder_freq = xponder_freq
-        self.freq_xlating_fir_filter_xxx_1_0.set_center_freq(-(self.capture_freq - self.xponder_freq ))
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_fftsink2_0, 0))
 
     def get_target_rate(self):
         return self.target_rate
 
     def set_target_rate(self, target_rate):
         self.target_rate = target_rate
-        self.qtgui_time_sink_x_0.set_samp_rate(self.target_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.capture_freq, self.target_rate)
 
-    def get_reader_freq(self):
-        return self.reader_freq
+    def get_target_freq(self):
+        return self.target_freq
 
-    def set_reader_freq(self, reader_freq):
-        self.reader_freq = reader_freq
+    def set_target_freq(self, target_freq):
+        self.target_freq = target_freq
 
-    def get_firdes_tap_low_xponder(self):
-        return self.firdes_tap_low_xponder
+    def get_samp_rate(self):
+        return self.samp_rate
 
-    def set_firdes_tap_low_xponder(self, firdes_tap_low_xponder):
-        self.firdes_tap_low_xponder = firdes_tap_low_xponder
-        self.freq_xlating_fir_filter_xxx_1_0.set_taps((self.firdes_tap_low_xponder))
-
-    def get_firdes_tap_low_reader(self):
-        return self.firdes_tap_low_reader
-
-    def set_firdes_tap_low_reader(self, firdes_tap_low_reader):
-        self.firdes_tap_low_reader = firdes_tap_low_reader
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
     def get_capture_freq(self):
         return self.capture_freq
 
     def set_capture_freq(self, capture_freq):
         self.capture_freq = capture_freq
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.capture_freq, self.target_rate)
-        self.freq_xlating_fir_filter_xxx_1_0.set_center_freq(-(self.capture_freq - self.xponder_freq ))
+        self.wxgui_fftsink2_0.set_baseband_freq(self.capture_freq)
+
+    def get_baud_rate(self):
+        return self.baud_rate
+
+    def set_baud_rate(self, baud_rate):
+        self.baud_rate = baud_rate
 
 
 def main(top_block_cls=top_block, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-    tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
+    tb.Start(True)
+    tb.Wait()
 
 
 if __name__ == '__main__':
